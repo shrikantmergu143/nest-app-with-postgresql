2f8bc534314452aff9ef86a7f525a81397133d67
shrikant <shrikant@appristine.in>
2025-12-18 22:22:30 +0530

    Pushing changes

--- src/constants/response.ts
+++ src/constants/response.ts
@@ -1,17 +1,17 @@
-import { HttpStatus } from '@nestjs/common';
-import { CONSTANT } from './message';
-import { Response } from 'express';
-import { createLogger, format, transports } from 'winston';
+import { HttpStatus } from "@nestjs/common";
+import { CONSTANT } from "./message";
+import { Response } from "express";
+import { createLogger, format, transports } from "winston";
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
-  level: 'info',
+  level: "info",
   format: format.combine(format.splat(), format.simple()),
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: `logs/logfile-${new Date().toLocaleDateString('es-CL')}.log`,
+      filename: `logs/logfile-${new Date().toLocaleDateString("es-CL")}.log`,
     }),
   ],
 });
@@ -34,17 +34,17 @@ const failureResponse = (error: any, res: Response) => {
   if (
     error.name &&
     [
-      'Error',
-      'TypeError',
-      'TypeORMError',
-      'QueryFailedError',
-      'EntityPropertyNotFoundError',
+      "Error",
+      "TypeError",
+      "TypeORMError",
+      "QueryFailedError",
+      "EntityPropertyNotFoundError",
     ].includes(error.name)
   ) {
-    logger.error(`${new Date().toLocaleString('es-CL')} ${error.message}`);
-    if (error.code && ['23505'].includes(error.code)) {
+    logger.error(`${new Date().toLocaleString("es-CL")} ${error.message}`);
+    if (error.code && ["23505"].includes(error.code)) {
       httpStatus = HttpStatus.BAD_REQUEST;
-      error.message = CONSTANT.ERROR.ALREADY_EXISTS('Record');
+      error.message = CONSTANT.ERROR.ALREADY_EXISTS("Record");
     } else {
       error.message = CONSTANT.ERROR.METHOD_NOT_ALLOWED;
     }
@@ -64,9 +64,17 @@ const validationError = (data: Data, res: Response) =>
     message: data.data,
     data: {},
   });
+const errorResponse = (data: Data, res: Response) => {
+  res.status(HttpStatus.BAD_REQUEST).json({
+    status: 0,
+    message: data.message ? data.message : CONSTANT.SUCCESS.DEFAULT,
+    data: data.data,
+  });
+};
 const response = {
   successResponse,
   failureResponse,
+  errorResponse,
   validationError,
 };
 export default response;
--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,41 +1,41 @@
-import { Body, Controller, Get, Next, Post, Req, Res } from '@nestjs/common';
-import { CreateUserDto } from '../models/create-user.dto';
-import { from, Observable } from 'rxjs';
-import response from 'src/constants/response';
-import { NextFunction, Request, Response } from 'express';
-import { UsersService } from '../services/auth.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Users } from '../models/user.entity';
-import { Repository } from 'typeorm';
-import { CONSTANT, MESSAGE } from 'src/constants/message';
+import { Body, Controller, Get, Next, Post, Req, Res } from "@nestjs/common";
+import { CreateUserDto } from "../models/create-user.dto";
+import { from, Observable } from "rxjs";
+import response from "src/constants/response";
+import { NextFunction, Request, Response } from "express";
+import { UsersService } from "../services/auth.service";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Users } from "../models/user.entity";
+import { Repository } from "typeorm";
+import { CONSTANT, MESSAGE } from "src/constants/message";
 
-@Controller('auth')
+@Controller("auth")
 export class UsersController {
   constructor(
     private usersService: UsersService,
     @InjectRepository(Users)
-    private readonly usersRepository: Repository<Users>,
+    private readonly usersRepository: Repository<Users>
   ) {}
-  @Post('signup/user')
+  @Post("signup/user")
   async signUp(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
     try {
       const user = await this.usersRepository.findOne({
         where: [{ email: createUserDto?.email }],
       });
       if (user) {
-        response.successResponse(
+        response?.errorResponse?.(
           { data: user, message: MESSAGE.USER_ALREADY_EXIST },
-          res,
+          res
         );
       } else {
         const result = await this.usersService.signUp(createUserDto);
         if (result) {
           return response.successResponse(
             {
-              message: CONSTANT.SUCCESS.COMPLETE_VERIFICATION('Signup'),
+              message: CONSTANT.SUCCESS.COMPLETE_VERIFICATION("Signup"),
               data: { id: result.id },
             },
-            res,
+            res
           );
         }
       }
@@ -43,7 +43,7 @@ export class UsersController {
       return response.failureResponse(error, res);
     }
   }
-  @Get('user/get')
+  @Get("user/get")
   async getUser(@Req() user: Request, @Res() res: Response) {
     const result = await this.usersService.findAll();
     response.successResponse(
@@ -51,7 +51,7 @@ export class UsersController {
         data: result,
         message: CONSTANT.SUCCESS.DEFAULT,
       },
-      res,
+      res
     );
   }
 }
